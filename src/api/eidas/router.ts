09e3b714afe2be3f8556544a5bc2edebd5bb0fda
Mauro Lucchini <maurolucchinidepompa@gmail.com>
2020-11-13 22:21:10 +0100

    remove request authorization

--- src/api/eidas/router.ts
+++ src/api/eidas/router.ts
@@ -23,10 +23,6 @@ class Router {
       cors(),
       async (req: express.Request, res: express.Response, next) => {
         try {
-          if (!req.params.authenticated)
-            throw new UnauthorizedError(UnauthorizedError.defaultTitle, {
-              detail: `The method '${req.method} ${req.path}' is not available for anonymous access`,
-            });
           const result = await Controller.EIDASsignature(req.body);
           res.status(201).json(result);
         } catch (error) {
@@ -40,10 +36,6 @@ class Router {
       cors(),
       async (req: express.Request, res: express.Response, next) => {
         try {
-          if (!req.params.authenticated)
-            throw new UnauthorizedError(UnauthorizedError.defaultTitle, {
-              detail: `The method '${req.method} ${req.path}' is not available for anonymous access`,
-            });
           if (!req.body.proof)
             throw new BadRequestError(BadRequestError.defaultTitle, {
               detail: ApiErrorMessages.BAD_CREDENTIAL_PARAMETERS,
